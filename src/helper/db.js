c960282bb0edd2240228b10862a6c000db79940a
Dorian-Garcia-pro <garcia.dorian.pro@gmail.com>
2022-01-17 16:59:49 +0100

    Update db.js

--- src/helper/db.js
+++ src/helper/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "chocochoc",
+  password: "azeaze1",
   database: "veille"
 })
 
