b10309faccf0095605ecdef5fd7787af910c8d42
Anouchka <anouchka.maurer@protonmail.com>
2022-01-20 15:50:00 +0100

    route like permet de vérifier si un user a déjà mis un like à un article

--- src/routes/like.js
+++ src/routes/like.js
@@ -6,6 +6,20 @@ Router.get("/", (req, res) => {
   res.send("i am on GET '/' ")
 })
 
+Router.post("/isliked", (req, res) => {
+  const sql = "SELECT EXISTS (SELECT * FROM veille.likes WHERE id_article IN ( ? ) AND id_user IN (?)) as isLiked"
+  const values = [
+    req.body.id_article,
+    req.body.id_user
+  ];
+
+  connection.query(sql, values, (err, result) => {
+    if (err) throw err
+    return res.status(200).send(result)
+  })
+  console.log("GET on like/isliked")
+})
+
 Router.post("/add", (req, res) => {
   console.log("req BODY",req.body)
   const sql = "INSERT INTO likes (id_article,id_user) VALUES (?,?)";
